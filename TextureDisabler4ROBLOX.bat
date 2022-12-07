9c74f60ac16e6a08439ca81554cd9edd90695605
notiamb1 <90900969+notiamb1@users.noreply.github.com>
2022-12-07 11:53:18 +0700

    Add files via upload

---
+++ TextureDisabler4ROBLOX.bat
@@ -0,0 +1,34 @@
+@echo off
+echo Type File Path for ROBLOX
+set /p filepath=
+cd %filepath%
+md %filepath%\ClientSettings
+cd %filepath%\PlatformContent\pc\textures\
+echo Deleting textures, press any key to continue
+pause
+echo deletion process starting
+rd /s brick
+rd /s cobblestone
+rd /s concrete
+rd /s corrodedmetal
+rd /s diamondplate
+rd /s fabric
+rd /s foil
+rd /s granite
+rd /s grass
+rd /s ice
+rd /s marble 
+rd /s metal
+rd /s pebble
+rd /s plastic
+rd /s sand
+rd /s slate 
+rd /s wood
+rd /s woodplanks
+rd /s fabric
+echo Deletion Process Complete
+cd %filepath%\ClientSettings
+echo {"FFlagDebugDisableOTAMaterialTexture":"true"}>%filepath%\ClientSettings\ClientAppSettings.json
+echo Made New File (ClientAppSettings.json) at %filepath%\ClientSettings
+echo Sucessfully deleted all textures, press any key to close.
+pause
